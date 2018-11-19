b34698203f0b013bc2af9c9bd3060cc6ee949d53
Thibyls Emory <bmartynchyk@gmail.com>
2018-11-19 04:26:38 +0300

    fix(argz.c) fix warnings messages

--- OOP_labs/OOP_lab_1/argz.c
+++ OOP_labs/OOP_lab_1/argz.c
@@ -10,15 +10,15 @@
 #include "argz.h"
 
 error_t argz_create_sep(const char *string, int sep, char **argz, size_t *argz_len) {
-
+	return OK;
 }
 
 size_t argz_count(const char *argz, size_t arg_len) {
-
+	return 0;
 }
 
 error_t argz_add(char **argz, size_t *argz_len, const char *str) {
-
+	return OK;
 }
 
 void argz_delete(char **argz, size_t *argz_len, char *entry) {
@@ -26,16 +26,16 @@ void argz_delete(char **argz, size_t *argz_len, char *entry) {
 }
 
 error_t argz_insert(char **argz, size_t *argz_len, char *before, const char *entry) {
-
+	return OK;
 }
 
 char* argz_next(char *argz, size_t argz_len, const char *entry) {
-
+	return NULL;
 }
 
 error_t argz_replace(char **argz, size_t *argz_len, const char *str, const char *with)
 {
-
+	return OK;
 }
 
 void argz_print(const char *argz, size_t argz_len) {

08fb93fc54c2a52b08d42427edbdb220446fcac3
Thibyls Emory <bmartynchyk@gmail.com>
2018-11-19 04:41:35 +0300

    add-code(argz_test.c) declare variables

--- OOP_labs/OOP_lab_1/argz_test.c
+++ OOP_labs/OOP_lab_1/argz_test.c
@@ -5,5 +5,10 @@
 #define STOP _getch()
 
 void main() {
+	int argz_len = 58;
+	char *const string = "SHELL=/bin/bash:usr=monty:PWD=/bin/monty:LANG=en_US.UTF-8";
+	char *argz = (char*)malloc(argz_len * sizeof(char));
+	int count = 0;
+
 	STOP;
 }
